79da8c1f84159231368e583e4125dfdc55118ed8
Dhruv Patel <Dhruv.21patel01@gmail.com>
2024-10-13 21:26:44 -0700

    minor changes on overflow

--- src/Components/Experience/ExpCard.jsx
+++ src/Components/Experience/ExpCard.jsx
@@ -1,8 +1,8 @@
 export default function ExpCard({ role, company, info, tech }) {
   return (
     <div className="w-full h-fit flex flex-col gap-[1rem]">
-      <h1 className="font-semibold text-[30px]">{company}</h1>
-      <h3 className="text-[20px] hover:text-black">{role}</h3>
+      <h1 className="font-semibold text-[15px] md:text-[30px]">{company}</h1>
+      <h3 className="text-[10px] md:text-[20px] hover:text-black">{role}</h3>
       <div className="contianer">
       {info.map((infomation,index) => (
         <p key={`info-${index}`}className="px-[1rem] text-start">{infomation}</p>
--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -17,7 +17,7 @@ export default function Experience(){
         animate:{
             opacity:1,
             x:0,
-            scale:1.5,
+            scale:1,
             transition :{
                 delay:0.2,
                 scale:{delay:0.4}
@@ -27,13 +27,13 @@ export default function Experience(){
     }
   
   return (
-    <div id="work" className="flex flex-col gap-[40px] w-full h-fit px-[0.5rem] ">
+    <div id="work" className="flex flex-col gap-[40px] w-full h-fit px-[0.5rem] scale-75 md:scale-100">
         <motion.div className="w-full flex items-center my-[5%]"
             variants={customanimatedvariants}
             initial={customanimatedvariants.initial}
             whileInView={customanimatedvariants.animate}
         >
-            <p className="md:text-[6rem] text-[2rem] text-white">Experience</p>
+            <p className="md:text-[6rem] text-[4rem] text-white w-full">Experience</p>
         </motion.div>
         <VerticalTimeline lineColor="grey">
             {
@@ -41,7 +41,7 @@ export default function Experience(){
                     return (
                         <VerticalTimelineElement key={idx}
                         className="text-wrap"
-                        contentStyle={{background:"#DBD7D2",color:"black",maxWidth:'90%',borderRadius:'10px',padding:"20px", boxShadow:"none", borderBottom:`4px solid ${exp["b-color"]}`}}
+                        contentStyle={{background:"#DBD7D2",color:"black",maxWidth:'90%',borderRadius:'10px',padding:"auto", boxShadow:"none", borderBottom:`4px solid ${exp["b-color"]}`}}
                         contentArrowStyle={{borderRight:"10px solid yellow"}}
                         icon={
                             <div className="w-full h-full flex items-center justify-center overflow-clip p-[10px]">
