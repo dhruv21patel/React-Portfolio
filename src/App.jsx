564a4a9724ed72a65239239c40618c4da38781ad
Dhruv Patel <Dhruv.21patel01@gmail.com>
2024-10-18 16:11:07 -0700

    last changes to scrolling effect

--- src/App.jsx
+++ src/App.jsx
@@ -10,77 +10,13 @@ import Routes from "./Routes/Routesall";
 import ScrollToTop from "./Routes/ScrollToTop";
 
 function App() {
-  // const [resize, setResize] = useState(true);
-
-  // useEffect(() => {
-  //   function handleresize(width) {
-  //     if (width <= 1000) {
-  //       window.location.reload();
-  //       setResize(() => !resize);
-  //     }
-  //   }
-
-  //   window.addEventListener("resize", () => handleresize(window.innerWidth));
-  //   return () => {
-  //     window.removeEventListener("resize", () =>
-  //       handleresize(window.innerWidth)
-  //     );
-  //   };
-  // }, [resize]);
-
-  // const circle =  document.getElementsByClassName('circle');
-
-  //  //mavgation animation
-  // document.body.addEventListener('mouseover', function(event) {
-
-  //   let target = '';
-
-  //   if( event.target.tagName == 'A')
-  //     {
-  //       target = event.target;
-
-  //       if (!target?.classList.contains('hover-border')) {
-  //         target.classList.add('hover-border');
-  //         circle[0].style.borderColor= 'transparent';
-  //       }
-  //     }
-
-  //     if( event.target.id == 'name')
-  //       {
-  //         document.getElementById('name').style.borderColor = 'rgb(160,114,219)';
-  //       }
-
-  // });
-
-  // document.body.addEventListener('mouseout', function(event) {
-  //   const target = event.target;
-
-  //   // Remove the active border class when mouse leaves the element
-  //   target.classList.remove('hover-border');
-  //   circle[0].style.borderColor = 'white';
-
-  //     if( event.target.id == 'name')
-  //     {
-  //       document.getElementById('name').style.borderColor = 'white';
-  //     }
-  // });
-
-  // function mousemovehandler(event){
-  //   circle[0].style.left = `${event.clientX}px`;
-  //   circle[0].style.top = `${event.clientY}px`;
-  // }
-  // document.addEventListener('mousemove',function(event){
-
-  // });
-  // Add event listener to the entire document
-
+ 
   return (
     <>
       <BrowserRouter>
-        {/* <ScrollRestoration/> */}
         <div className="h-screen flex flex-col">
           <Navigation />
-          <div className="flex-grow h-fit ">
+          <div className="flex-grow h-fit overflow-y-scroll ">
             <ScrollToTop/>
             <Routes />
           </div>
--- src/Components/Projects/ProjectCard/ProjectProjectCard.jsx
+++ src/Components/Projects/ProjectCard/ProjectProjectCard.jsx
@@ -2,18 +2,31 @@ import { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { motion } from "framer-motion";
 import projects from "../../../Utils/project";
+import { div } from "framer-motion/client";
 
 export default function ProjectProjectCard() {
   const { id } = useParams();
   const current_project = projects[id];
 
+  const topRef = useRef(null);
+
+  useEffect(() => {
+    if (topRef.current) {
+      topRef.current.scrollIntoView({ behavior: 'instant' });
+    }
+  }, []);
+
   return (
-    <div className="h-auto w-full flex flex-col items-center justify-center  gap-[4rem]">
-      <div className=" flex items-center justify-center ">
-        <div className="container w-[60dvh] aspect-square flex items-center">
+    <div  ref={topRef} className="h-auto flex flex-grow flex-col justify-center">
+
+      <div className="h-auto flex justify-center ">
+
+        <div className="relative w-[60dvh] aspect-square flex items-center justify-center">
           <motion.div
-            className=" relative h-[100%] object-contain flex items-center overflow-y-hidden"
-            initial={{ height: "0%", originY: 0 }}
+            className="absolute min-w-full h-full flex items-center backdrop-brightness-50 overflow-hidden"
+            initial={{
+              height: "0%",
+            }}
             animate={{
               height: "100%",
               transition: {
@@ -22,53 +35,60 @@ export default function ProjectProjectCard() {
               },
             }}
           >
-            <motion.img
-              src={current_project.backgroundImage}
-              className="object-contain brightness-50"
-              alt=""
-            />
-            <h1
-              className="absolute inset-auto h-fit w-full text-center text-[3rem]"
-              style={{ fontWeight: "600" }}
-            >
-              {current_project.name}
-            </h1>
+            <div className="relative h-full flex flex-grow justify-center items-center">
+              <img
+                src={current_project.backgroundImage}
+                alt=""
+                className="object-contain"
+              />
+              <h1
+                className="absolute inset-auto h-full w-full backdrop-brightness-50 text-center text-[3rem] flex items-center justify-center"
+                style={{ fontWeight: "600" }}
+              >
+                {current_project.name}
+              </h1>
+            </div>
           </motion.div>
         </div>
       </div>
 
-      <div className=" h-fit flex justify-center">
-        <div className="container flex flex-col gap-[1rem]">
+      {/* project information details */}
+      <div className="h-fit flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
           <h1 className="text-[2.7rem]">OVERVIEW</h1>
+
           <p
             className="text-[1.5rem] px-[4%]"
             style={{ fontFamily: "var(--para)", fontWeight: "200" }}
           >
             {current_project.infromation}
           </p>
-        </div>
-      </div>
 
-      <div className="container flex items-center justify-center">
-        <div className="container flex flex-col gap-[2rem] justify-center items-center ">
           <div className="container lg:border-b-2 lg:border-b-white w-[100%] text-start py-[1.5rem] px-[1rem] lg:p-[3rem] flex flex-col md:flex-row justify-between ">
-            <h1 className="text-[2.5rem] text-nowrap w-full md:w-[40%] ">Technologies</h1>
+            <h1 className="text-[2.5rem] text-nowrap w-full md:w-[40%] ">
+              Technologies
+            </h1>
             <ul
               className="text-[2rem] w-full px-[15%]"
               style={{ fontFamily: "var(--primary)" }}
             >
-              {current_project.Technologies.map((tech,idx) => (
-                <li key={idx} className="text-[1.3rem]">{tech}</li>
+              {current_project.Technologies.map((tech, idx) => (
+                <li key={idx} className="text-[1.3rem]">
+                  {tech}
+                </li>
               ))}
             </ul>
           </div>
+
           <div className="container lg:border-b-2 lg:border-b-white w-[100%] text-start py-[1.5rem] px-[1rem] lg:p-[3rem] flex flex-col md:flex-row justify-between ">
-            <h1 className="text-[2.5rem] text-nowrap w-full md:w-[40%] ">GOAL</h1>
+            <h1 className="text-[2.5rem] text-nowrap w-full md:w-[40%] ">
+              GOAL
+            </h1>
             <ul
               className="text-[2rem] gap-[1rem] w-full px-[15%] "
               style={{ fontFamily: "var(--primary)" }}
             >
-              {current_project.Goals.map((goal,idx) => (
+              {current_project.Goals.map((goal, idx) => (
                 <li key={idx}>
                   {/* Add a unique key for each item */}
                   <p className="font-bold text-[1.3rem]">{goal[0]}</p>
@@ -77,19 +97,23 @@ export default function ProjectProjectCard() {
               ))}
             </ul>
           </div>
-          {/* <div className="container border-b-2 border-b-white"></div> */}
         </div>
       </div>
 
-      <div className="container flex flex-col lg:flex-row flex-wrap items-center justify-center pt-[4rem] gap-[2rem]">
-        {current_project.images.map((img,idx) => {
-          return (
-            <div key={idx} className="w-[60%] aspect-square flex items-center">
-              <img src={img} className="object-cover h-auto" alt="" />
-            </div>
-          );
-        })}
-      </div>
+      <div className="w-full h-auto flex justify-center">
+          <div className="container flex flex-col lg:flex-row flex-wrap items-center justify-center gap-[2rem] h-fit">
+            {current_project.images.map((img, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className="w-[60%] aspect-square flex items-center"
+                >
+                  <img src={img} className="object-cover h-auto" alt="" />
+                </div>
+              );
+            })}
+          </div>
+        </div>
     </div>
   );
 }
